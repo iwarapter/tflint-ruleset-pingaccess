7d9e80ad5d051bc74b051fa37bd7817247336750
Iain Adams <iwarapter@hotmail.com>
2021-07-16 14:10:20 +0100

    update for goreleaser ldflag injected values

--- main.go
+++ main.go
@@ -6,11 +6,15 @@ import (
 	"github.com/terraform-linters/tflint-plugin-sdk/tflint"
 )
 
+var (
+	version = ""
+)
+
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
 		RuleSet: &tflint.BuiltinRuleSet{
 			Name:    "pingaccess",
-			Version: "0.1.0",
+			Version: version,
 			Rules: []tflint.Rule{
 				rules.NewPingFederateRunetimeSingletonRule(),
 				rules.NewSiteSkipHostnameVerificationRule(),
